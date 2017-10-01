d294335a0af116c6db0cf35db6a3cea9b4f325c2
Sunny Mishra <mishrasunny174@gmail.com>
2017-10-01 21:22:03 +0530

    added progress bar

--- source.cpp
+++ source.cpp
@@ -5,12 +5,16 @@
 
 using namespace std;
 
+typedef long unsigned int filePosition; //to index the file
+
 class XOREncryptor
 {
+    void showProgressBar(filePosition ,filePosition); //it will display progress bar
+    filePosition getFileSize(string); //to get the complete file size to calculate progress
 public:
     XOREncryptor(){}
-    void encrypt(string fileName,string key);
-    void decrypt(string fileName,string key);
+    void encrypt(string ,string);
+    void decrypt(string ,string);
 };
 
 int main(int argc,char* argv[])
@@ -31,7 +35,7 @@ int main(int argc,char* argv[])
     }
     else
         cout<<"Usage: "<<argv[0]<<" <encrypt/decrypt> <filename> <key>"<<endl;
-    cout<<"Enter enter key to continue..."<<endl;
+    cout<<"Press enter to continue..."<<endl;
     cin.get();
     return 0;
 }
@@ -40,21 +44,23 @@ void XOREncryptor::encrypt(string fileName,string key)
 {
     ifstream iFile;
     ofstream oFile;
-    string outFile=fileName+".xre";
+    string outFileName=fileName+".xre";
     int i=0;
     char temp;
     char *charptr=&temp;
+    filePosition fullFileSize=getFileSize(fileName);
     try
     {
         iFile.open(fileName,ios::binary|ios::in);
-        oFile.open(outFile,ios::binary|ios::out|ios::trunc);
+        oFile.open(outFileName,ios::binary|ios::out|ios::trunc);
         if(!(oFile.is_open()||iFile.is_open()))
             throw 1;
-        cout<<"Encrypting File... Please Wait!"<<endl;
+        cout<<"Encrypting File... Please wait!!!"<<endl;
         while(iFile.read(charptr,1))
         {
             temp=temp^key[(i++)%key.length()];
             oFile.put(temp);
+            showProgressBar(iFile.tellg(),fullFileSize);
         }
         iFile.close();
         oFile.close();
@@ -64,29 +70,31 @@ void XOREncryptor::encrypt(string fileName,string key)
         cout<<"ERROR during opening file."<<endl;
         exit(1);
     }
-    cout<<"File encrypted successfully!!!"<<endl;
+    cout<<"\nFile encrypted successfully!!!"<<endl;
 }
 
 void XOREncryptor::decrypt(string fileName,string key)
 {
     ifstream iFile;
     ofstream oFile;
-    string outFile;
-    outFile=fileName.substr(0,fileName.length()-4);
+    string outFileName;
+    outFileName=fileName.substr(0,fileName.length()-4);
     int i=0;
     char temp;
     char *charptr=&temp;
+    filePosition fullFileSize=getFileSize(fileName);
     try
     {
         iFile.open(fileName,ios::binary|ios::in);
-        oFile.open(outFile,ios::binary|ios::out|ios::trunc);
+        oFile.open(outFileName,ios::binary|ios::out|ios::trunc);
         if(!(oFile.is_open()||iFile.is_open()))
             throw 1;
-        cout<<"Decrypting File... Please Wait!"<<endl;
+        cout<<"Decrypting File... Please wait!!!"<<endl;
         while(iFile.read(charptr,1))
         {
             temp=temp^key[(i++)%key.length()];
             oFile.put(temp);
+            showProgressBar(iFile.tellg(),fullFileSize);
         }
         iFile.close();
         oFile.close();
@@ -96,5 +104,26 @@ void XOREncryptor::decrypt(string fileName,string key)
         cout<<"ERROR during opening file."<<endl;
         exit(1);
     }
-    cout<<"File decrypted successfully!!!"<<endl;
+    cout<<"\nFile decrypted successfully!!!"<<endl;
+}
+
+void XOREncryptor::showProgressBar(filePosition completed,filePosition total)
+{
+    float progress=static_cast<float>(completed)/total;
+    int width=50;
+    cout<<"[";
+    for(int i=1;i<width;i++)
+    {
+        if(i<width*progress)
+            cout<<"=";
+        else
+            cout<<" ";
+    }
+    cout<<"] "<<static_cast<int>(progress*100)<<"%\r";
+}
+
+filePosition XOREncryptor::getFileSize(string fileName)
+{
+    ifstream file(fileName,ios::in|ios::ate|ios::binary);
+    return file.tellg();
 }
